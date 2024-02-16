48b635e8bae19f8f4e128c0f86bb250d3fe87f28
Yuri Gonçalves <54912331+yokatsu@users.noreply.github.com>
2024-02-16 11:01:35 -0300

    add google analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,6 +60,16 @@ export default function Index({
       <PortfolioSection portfolioData={portfolioData} />
       <BlogSection blogData={blogData} />
       <ContactSection contactData={contactData} />
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KJGJKYN5P7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KJGJKYN5P7');
+</script>
     </>
   );
 }
